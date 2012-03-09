a85ae303b9236c0659215fd1a8a2f86715c1124a
Aline de Oliveira Freitas <aline.do.freitas@gmail.com>
2012-03-09 13:31:12 -0300

    Updated README file and added the instalations script

--- install.sh
+++ install.sh
@@ -1,2 +1,9 @@
+echo "ATENÇÃO!! Esse script assume que o usuário tem instalado em seu computador a versão mais nova do RVM. Caso não tenha, favor interromper o script e primeiro visite http://beginrescueend.com/rvm/install/ para receber instruções de como instalar o RVM em seu computador."
+
+rvm install ruby-1.9.2-p290
+rvm use ruby-1.9.2-p290
+rvm gem install rails --version=3.0.10
 bundle install
 
+
+echo "Se o programa rodou sem erros até então, vá em frente e rode: rails server para rodar a app!"
